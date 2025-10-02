c4914442dd92c4a0529117364f978d1e6a693798
Pranav-learner <pranavgupta32006@gmail.com>
2025-10-02 14:10:44 +0530

    Refactor RightSidebar and ChatContainer to conditionally render online user status; update HomePage to utilize ChatContext for selectedUser management.

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import assets, { imagesDummyData } from "../assets/assets";
+import { useContext } from "react";
+import { ChatContext } from "../../context/ChatContext";
+import { AuthContext } from "../../context/AuthContext";
+import { useState } from "react";
 
-function RightSidebar({ selectedUser }) {
+const RightSidebar = () => {
+  const { selectedUser, messages } = useContext(ChatContext);
+
+  const { logout, onlineUser } = useContext(AuthContext);
+
+  const [msgImages, setMsgImages] = useState([]);
+
+  // get all images from  the messages and set them to state
+
+  useEffect(() => {
+    setMsgImages(messages.filter((msg) => msg.image).map((msg) => msg.image));
+  }, [messages]);
+
   return (
     selectedUser && (
       <div
@@ -16,7 +32,9 @@ function RightSidebar({ selectedUser }) {
             className="w-20 aspect-[1/1] rounded-full"
           />
           <h1 className="px-10 text-xl font-medium mx-auto flex items-center gap-2">
-            <p className="w-2 h-2 rounded-full bg-green-500"></p>
+            {(onlineUser || []).includes(selectedUser?._id) && (
+              <p className="w-2 h-2 rounded-full bg-green-500"></p>
+            )}
             {selectedUser?.fullName}
           </h1>
           <p className="px-10 mx-auto">{selectedUser?.bio}</p>
@@ -27,7 +45,7 @@ function RightSidebar({ selectedUser }) {
         <div className="p-5 text-xs">
           <p>Media</p>
           <div className="mt-2 max-h-[200px] overflow-y-scroll grid grid-cols-2 gap-4 opacity-80">
-            {imagesDummyData.map((img, index) => (
+            {msgImages.map((img, index) => (
               <div
                 key={index}
                 onClick={() => window.open(img)}
@@ -38,12 +56,15 @@ function RightSidebar({ selectedUser }) {
             ))}
           </div>
         </div>
-        <button className="absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer">
+        <button
+          onClick={() => logout()}
+          className="absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer"
+        >
           Logout
         </button>
       </div>
     )
   );
-}
+};
 
 export default RightSidebar;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useState } from "react";
+import { useContext } from "react";
 import Sidebar from "../components/Sidebar";
 import ChatContainer from "../components/ChatContainer";
 import RightSidebar from "../components/RightSidebar";
+import { ChatContext } from "../../context/ChatContext";
 
 function HomePage() {
-  const [selectedUser, setSelectedUser] = useState(false);
+  const { selectedUser } = useContext(ChatContext);
 
   return (
     <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
@@ -19,10 +21,7 @@ function HomePage() {
       >
         <Sidebar />
         <ChatContainer />
-        <RightSidebar
-          selectedUser={selectedUser}
-          setSelectedUser={setSelectedUser}
-        />
+        <RightSidebar />
       </div>
     </div>
   );
